17e9b17cb081f52fd6faffb34e69e884d049eaa6
cdebacker <corey.debacker@gmail.com>
2024-09-18 11:41:01 -0500

    Basic example of a feature file for an external module
    - Included generic step definitions are just placeholders, should be able to be replaced with existing RCTF steps
    - Added a Cypress environmental variable and  before() block to allow users to define which installed external modules should be enabled/disabled before test runs

--- cypress/features/css_injector.feature
+++ cypress/features/css_injector.feature
@@ -3,15 +3,45 @@ Feature: CSS Injector EM
   As a REDCap end user
   I want to see that the "REDCap CSS Injector" External Module works as expected
 
-  Scenario: Configure module via Module Manager / Control Center
+  Scenario: Enable module on all projects by default
     Given I login to REDCap with the user "Test_Admin"
 
-    When I click on the link labeled "Control Center"
-    And I click on the link labeled exactly "Manage"
-    Then I should see "REDCap CSS Injector"
-    
-   When I click on the button labeled exactly "Configure"
-   Then I should see "Configure Module: REDCap CSS Injector"
+    # And II test something
 
-  Scenario: Hide module from non-admins on projects' lists of enabled modules
-    Given I check the box labeled "Hide from non-admins on projects' lists of enabled modules"
+    And II click the link labeled "Control Center"
+    And II click the link labeled "Manage"
+    # Then I should see "REDCap CSS Injector"
+    
+    And II click the button labeled "Configure"
+    # Then I should see "Configure Module: REDCap CSS Injector"
+
+    And II check the box labeled "Enable module on all projects by default:"
+    And II click the button labeled "Save"
+
+    # Then II should see "Enabled for All Projects"
+    # Then I should see "Enabled for All Projects" in the table row containing "REDCap CSS Injector"
+
+  Scenario: Create a project
+    Given II click the link labeled "New Project"
+    And II enter "Test Project" into the field labeled "Project title:"
+    And II select "Practice / Just for fun" from the dropdown labeled "Project's purpose:"
+    And II click the button labeled "Create Project"
+
+  Scenario: Configure some CSS for the project
+    Given II click the link labeled "Manage"
+    And II click the button labeled "Configure"
+    And II check the box labeled "Enabled:"
+    And II choose "All" from the radio buttons labeled "Apply to:"
+    # Module seems to convert color codes to rgb format
+    # And II enter "button#submit-btn-saverecord {background-color: #fca90d !important;}" into the field labeled "CSS:"
+    And II enter "button#submit-btn-saverecord {background-color: rgb(252, 169, 13) !important;}" into the field labeled "CSS:"
+    # Specific shade of orange unlikely to be already used by REDCap
+    And II click the button labeled "Save"
+
+  Scenario: Create a record and verify the CSS is applied
+    # Page reloads after saving config, so need to wait
+    Given II wait 500 milliseconds
+    Given II click the link labeled "Add / Edit Records"
+    And II click the button labeled "Add new record"
+    # Then the elements selected by "button#submit-btn-saverecord" should have the style "background-color" with the value "#fca90d"
+    Then the elements selected by "button#submit-btn-saverecord" should have the style "background-color" with the value "rgb(252, 169, 13)"
--- cypress/support/step_definitions/corey.js
+++ cypress/support/step_definitions/corey.js
@@ -13,9 +13,9 @@ const { Given } = require('@badeball/cypress-cucumber-preprocessor')
 Given("I click to enable the module named {string} with version {string}", (moduleName, version) => {
 	cy.get('table#external-modules-disabled-table').should('be.visible').then(($el) => {
         // Select the appropriate version from the dropdown
-        cy.wrap($el).find(`tr:contains(${moduleName})`).find('select').select(version)
+        cy.wrap($el).find(`tr:contains(${moduleName})`).find('select').select(version);
         // Click the Enable button
-        cy.wrap($el).find(`tr:contains(${moduleName})`).find('button:contains("Enable")').click()
+        cy.wrap($el).find(`tr:contains(${moduleName})`).find('button:contains("Enable")').click();
     })
 })
 
@@ -25,56 +25,174 @@ Given("I click to enable the module named {string} with version {string}", (modu
 Given("I enable the External Module named {string} with version {string}", (moduleName, version) => {
     // Navigate to Control Panel
 })
+  
+/**
+ * @description Placeholder generic for entering text into fields until I can figure out how to use existing steps.
+ * Existing text is not cleared before entering new text.
+ */
+Given("II enter {string} into the field labeled {string}", (text, label) => {
+    // Find visible elements containing the text and not containing any visible children that also contain the text
+    // cy.get(`:contains(${label}):visible:not(:has(:contains(${label}):visible))`)
+    // cy.contains(`:visible:not(:has(:contains(Working...)))`, "Working...");
 
+    cy.findVisibleText(label)
+        .closest(':has(input[type="text"]:visible,textarea:visible):visible')
+        .find('input[type="text"]:visible,textarea:visible')
+        .first()
+        .type(text, {parseSpecialCharSequences: false});
+})
 
-// Before running any steps, make AJAX requests to enable the modules specified in the environment variable
-before(() => {
-    // Get the list of modules from the environment variable
-    const modules = Cypress.env('external_modules');
+Given("II select {string} from the dropdown labeled {string}", (text, label) => {
+    cy.findVisibleText(label)
+        .closest(':has(select:visible):visible')
+        .find('select:visible')
+        .first()
+        .select(text);
+})
 
-    // Log in as admin and get CSRF token
-    cy.set_user_type('admin');
-    cy.fetch_login();
-    
-    cy.window().then((win) => {
+// Assumes radio inputs have corresponding <label> as immediate siblings, seems to hold true across REDCap at a glance
+Given("II choose {string} from the radio buttons labeled {string}", (text, label) => {
+    cy.findVisibleText(label)
+        .closest(':has(input[type="radio"]:visible):visible')
+        .find('input[type="radio"]:visible')
+        .filter((_, element) => {
+            return Cypress.$(element).next().text().trim() === text;
+        })
+        .check();
+})
 
-        // Get the CSRF token from the window object
-        const csrfToken = win.redcap_csrf_token;
+Given("II check the box labeled {string}", (label) => {
+    cy.findVisibleText(label)
+        .closest(':has(input[type="checkbox"]:visible):visible')
+        .find('input[type="checkbox"]:visible')
+        .first()
+        .check();
+})
 
-        // Iterate over each module to enable or disable
-        modules.forEach((mod) => {
-            if (mod.enabled) {
-                // Make a POST request to enable the module
-                cy.request({
-                    method: 'POST',
-                    url: '/redcap_v' + Cypress.env('redcap_version') + '/ExternalModules/manager/ajax/enable-module.php',
-                    form: true,
-                    body: {
-                        prefix: mod.name,
-                        version: mod.version,
-                        redcap_csrf_token: csrfToken
-                    }
-                })
-                    .then((response) => {
-                        const body = JSON.parse(response.body);
-                        expect(body.message).to.eq('success');
-                    })
-            } else {
-                // Make a POST request to disable the module
-                cy.request({
-                    method: 'POST',
-                    url: '/redcap_v' + Cypress.env('redcap_version') + '/ExternalModules/manager/ajax/disable-module.php',
-                    form: true,
-                    body: {
-                        module: mod.name,
-                        redcap_csrf_token: csrfToken
-                    }
-                })
-                    .then((response) => {
-                        // Response seems to always return 
-                        expect(response.body).to.eq('success');
-                    })
+Given("II click the button labeled {string}", (label) => {
+    cy.findVisibleText(label)
+        .then(($el) => {
+            // If the element is a button, click it
+            if ($el.is('button')) {
+                cy.wrap($el).click();
+            }
+            // Otherwise, find the closest button and click it
+            else {
+                cy.wrap($el)
+                    .closest(':has(button:visible):visible')
+                    .find('button:visible')
+                    .first()
+                    .click();
             }
         })
+})
+
+Given("II click the link labeled {string}", (label) => {
+    cy.findVisibleText(label)
+        .then(($el) => {
+            // If the element is a link, click it
+            if ($el.is('a')) {
+                cy.wrap($el).click();
+            }
+            // Otherwise, find the closest link and click it
+            else {
+                cy.wrap($el)
+                    .closest(':has(a:visible):visible')
+                    .find('a:visible')
+                    .first()
+                    .click();
+            }
+        })
+})
+
+Given("II should see {string}", (text) => {
+    cy.findVisibleText(text, false);
+})
+
+Given("the element(s) selected by {string} should have the style {string} with the value {string}", (selector, property, value) => {
+    cy.get(selector)
+        .should('have.css', property, value);
+})
+
+Given("II wait {int} milliseconds", (ms) => {
+    cy.wait(ms);
+})
+
+Cypress.Commands.add('findVisibleText', (text, exactMatch = true) => {
+    cy.get('body')
+        .find('*')
+        .filter((_, element) => {
+            const $element = Cypress.$(element);
+            
+            // Check if the element directly contains the exact text
+            const directTextMatch = $element.contents().filter(function () {
+                if (exactMatch) {
+                    return this.nodeType === 3 &&
+                        this.nodeValue.trim() === text;
+                } else {
+                    return this.nodeType === 3 &&
+                        this.nodeValue.includes(text);
+                }
+            }).length > 0;
+  
+            // Ensure the element is visible and directly contains the text
+            return directTextMatch && $element.is(':visible');
+        })
+        .should((visibleElements) => {
+            expect(visibleElements.length, `Expected at least one visible element containing text: "${text}"`).to.be.greaterThan(0);
+        });
+});
+
+// Provides a way to enable or disable external modules without having to use the GUI, currently requires current user to be admin
+Cypress.Commands.add('toggle_external_module', (moduleName, version, enable = true) => {
+    cy.window().then((win) => {
+        // Get CSRF token from the window object
+        const csrfToken = win.redcap_csrf_token;
+
+        if (enable) {
+            // Make a request to enable the module
+            cy.request({
+                method: 'POST',
+                url: '/redcap_v' + Cypress.env('redcap_version') + '/ExternalModules/manager/ajax/enable-module.php',
+                form: true,
+                body: {
+                    prefix: moduleName,
+                    version: version,
+                    redcap_csrf_token: csrfToken
+                }
+            })
+                .then((response) => {
+                    const body = JSON.parse(response.body);
+                    expect(body.message).to.eq('success');
+                })
+        } else {
+            // Make a request to disable the module
+            cy.request({
+                method: 'POST',
+                url: '/redcap_v' + Cypress.env('redcap_version') + '/ExternalModules/manager/ajax/disable-module.php',
+                form: true,
+                body: {
+                    module: moduleName,
+                    redcap_csrf_token: csrfToken
+                }
+            })
+                .then((response) => {
+                    // Response seems to always have 200 status, so check for success message in body
+                    expect(response.body).to.eq('success');
+                })
+        }
+    });
+})
+
+// Before running any steps, enable the modules specified in the environment variable
+before(() => {
+    const modules = Cypress.env('external_modules');
+
+    // Log in as admin
+    cy.set_user_type('admin');
+    cy.fetch_login();
+
+    modules.forEach((mod) => {
+        cy.toggle_external_module(mod.name, mod.version, mod.enabled);
     });
 })
